764e79db5d2d5d44efad8992a075bb99aa2c1aea
shihao wang <shihao.wang@hand-china.com>
2020-05-08 21:20:42 +0800

    feat: 加入注释

--- src/helpers.ts
+++ src/helpers.ts
@@ -25,24 +25,38 @@ export interface ModuleContainerProps {
 function getRoot(): Container {
   return _global[key] as Container;
 }
-
+/**
+ * Create root IOC container
+ * @param {RootContainerProps} props
+ */
 export function createRootContainer(props: RootContainerProps): void {
   const container = new Container({ ...props, namespace: "root" });
   _global[key] = container;
 }
 
+/**
+ * 创建一个子模块
+ * @param {Container} props
+ */
 export function createModuleContainer(props: ModuleContainerProps): Container {
-  const root = getRoot();
+  let root = getRoot();
   const container = new Container(props);
   if (!root) {
     createRootContainer({
       providers: [],
       modules: [container]
-    })
+    });
+    root = getRoot();
   }
+  root.registerModule(container, props.namespace)
   return container;
 }
 
+/**
+ * 全局注入器函数
+ * @param name {String}
+ * @param scope {String}
+ */
 export function inject<T>(name: any, scope?: string) {
   const rootContainer = _global[key] as Container;
   if (!rootContainer) {
@@ -54,6 +68,10 @@ export function inject<T>(name: any, scope?: string) {
   return rootContainer.getInstance(name) as T;
 }
 
+/**
+ * 创建于命名空间绑定的工具
+ * @param {String} namespace
+ */
 export function createNamespaceHelpers(namespace: string) {
   return {
     inject: (name: any) => inject(name, namespace),
